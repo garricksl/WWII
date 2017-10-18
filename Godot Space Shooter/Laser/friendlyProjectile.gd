a55b6e6c62b24385c820cf84bc39700dbc1ad7e6
misha <misha.behey@gmail.com>
2017-10-17 18:37:26 -0700

    updated background color again

--- Godot Space Shooter/Laser/friendlyProjectile.gd
+++ Godot Space Shooter/Laser/friendlyProjectile.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 
 var PROJECTILE_SPEED = 400
 var shoot_dir = Vector2()
+var increaseEnemyCounter = 2
 
 func _ready():
 	set_pos(Vector2(100, 100))
@@ -21,8 +22,10 @@ func _on_Area2D_body_enter( body ):
 	if(body.is_in_group("enemy") and body.is_in_group("ship")):
 		body.queue_free()
 		queue_free()
-		var pos = Vector2(randf()*1000 + 100, randf()*2000 + 50)
-		var ship = preload("res://Godot Space Shooter/Enemy/enemy.tscn")
-		var obj = ship.instance()
-		obj.set_pos(pos)
-		get_parent().add_child(obj)
+		for i in range(0, increaseEnemyCounter):
+			var pos = Vector2(randf()*1000 + randf() * 100, randf()*2000 + randf()*50)
+			var ship = preload("res://Godot Space Shooter/Enemy/enemy.tscn")
+			var obj = ship.instance()
+			obj.set_pos(pos)
+			get_parent().add_child(obj)
+			increaseEnemyCounter += 1
